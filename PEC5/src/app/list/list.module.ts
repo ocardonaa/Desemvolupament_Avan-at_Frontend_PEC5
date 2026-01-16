6e96b7abd78a87b87264d0ab5409d8e5080b4773
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2026-01-16 19:40:51 +0100

    done menu list

--- PEC5/src/app/list/list.module.ts
+++ PEC5/src/app/list/list.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { ListRoutingModule } from './list-routing.module';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 
 
 @NgModule({
   declarations: [ListComponent],
   imports: [
-    CommonModule, ListRoutingModule
+    CommonModule, ListRoutingModule, ScrollingModule
   ]
 })
 export class ListModule { }
--- PEC5/src/app/list/list/list.component.ts
+++ PEC5/src/app/list/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ReadCSVService } from '../../services/read-csv.service';
+import { StudentDTO } from '../../models/student-dto.model';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  students: any[] = [];
 
+  constructor(private readcsv: ReadCSVService) {
+  }
+
+  ngOnInit() {
+    this.readcsv.getCsvData('/assets/notas_alumnos-1.csv').subscribe({
+      next: (data) => {
+        data.forEach(student => {
+          const newStudent = new StudentDTO(student.ID_Alumno, student.Nombre, student.Apellidos, student.Sexo, student.Nota_Final, student.Faltas_Asistencia);
+          this.students.push(newStudent);
+        });
+      },
+      error: (err) => console.error('Error parsing CSV:', err)
+    });
+  }
 }
