7dbc242b4e37d4be9ff6b6b948efa03a795451ce
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2026-01-18 20:44:28 +0100

    menu statistics done

--- PEC5/src/app/stadistics/gender/gender.component.ts
+++ PEC5/src/app/stadistics/gender/gender.component.ts
@@ -1,14 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { ReadCSVService } from '../../services/read-csv.service';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-gender',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective, CommonModule],
   templateUrl: './gender.component.html',
   styleUrl: './gender.component.scss'
 })
 export class GenderComponent {
+  males = 0;
+  females = 0;
+  isBrowser: boolean = false;
 
+  chartData: any = {};
+
+  chartOptions: any = {};
+
+  constructor(private readcsv: ReadCSVService, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId); //usado por un error en la consola proveniente de Canvas
+  }
+
+  createChart() {
+    this.chartData = {
+      labels: ['Male Students', 'Female Students'],
+      datasets: [
+        {
+          data: [this.males, this.females],
+          label: 'Students Resume Data'
+        }
+      ]
+    }
+    this.chartOptions = {
+      responsive: true
+    }
+  }
+
+  ngOnInit() {
+    this.readcsv.getCsvData('/assets/notas_alumnos-1.csv').subscribe({
+      next: (data) => {
+        data.forEach(student => {
+          if (student.Sexo === 'F') {
+            ++this.females;
+          }
+          else {
+            ++this.males;
+          }
+        });
+        this.createChart();
+      },
+      error: (err) => console.error('Error parsing CSV:', err)
+    });
+  }
 
 }
--- PEC5/src/app/stadistics/general-results/general-results.component.ts
+++ PEC5/src/app/stadistics/general-results/general-results.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { ReadCSVService } from '../../services/read-csv.service';
+import { BaseChartDirective } from 'ng2-charts';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-general-results',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective, CommonModule],
   templateUrl: './general-results.component.html',
   styleUrl: './general-results.component.scss'
 })
 export class GeneralResultsComponent {
+  passed_students = 0;
+  failed_students = 0;
+  isBrowser: boolean = false;
 
+  chartData: any = {};
+
+  chartOptions: any = {};
+
+  constructor(private readcsv: ReadCSVService, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  createChart() {
+    this.chartData = {
+      labels: ['Failed Students', 'Passed Students'],
+      datasets: [
+        {
+          data: [this.failed_students, this.passed_students],
+          label: 'Passed/Failed Resume Data'
+        }
+      ]
+    }
+    this.chartOptions = {
+      responsive: true
+    }
+  }
+
+  ngOnInit() {
+    this.readcsv.getCsvData('/assets/notas_alumnos-1.csv').subscribe({
+      next: (data) => {
+        data.forEach(student => {
+          if (student.Nota_Final < 5) {
+            ++this.failed_students;
+          }
+          else {
+            ++this.passed_students;
+          }
+        });
+        this.createChart();
+      },
+      error: (err) => console.error('Error parsing CSV:', err)
+    });
+  }
 }
